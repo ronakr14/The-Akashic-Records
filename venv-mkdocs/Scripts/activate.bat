cfcebb83b6e54461b483256a3b9b724122e473d4
Ronak Rathore <ronak.rathore05@gmail.com>
2025-07-13 20:41:26 +0530

    Add meeting notes template and update virtual environment configuration

--- venv-mkdocs/Scripts/activate.bat
+++ venv-mkdocs/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ronak\Downloads\Personal Work\The-Akashic-Records\venv-mkdocs
+set VIRTUAL_ENV=D:\Personal\The-Akashic-Records\venv-mkdocs
 
 if not defined PROMPT set PROMPT=$P$G
 
